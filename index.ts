f6749d2600d877229ae8a5c32ed44dc387c39e4a
Sourav Das <sdunity12061999@gmail.com>
2022-03-30 21:24:48 +0530

    Server making complete

--- index.ts
+++ index.ts
@@ -1,1 +1,19 @@
-console.log("ok");
+// All the Packages used here
+const express = require("express");
+const http=require("http");
+require("dotenv").config();
+// All the import from other files
+
+const PORT=process.env.PORT || 8080;
+
+const app=express();// Express app initialization
+
+
+const server=http.createServer(app);
+
+server.listen(PORT,()=>{
+    console.log("Server started at port :-",PORT);
+    
+})
+
+
