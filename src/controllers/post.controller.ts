1644b41fa2ccd8b5ea03d17856419d0ac947d6cf
Sourav Das <sdunity12061999@gmail.com>
2022-03-31 11:19:16 +0530

    post by user added

--- src/controllers/post.controller.ts
+++ src/controllers/post.controller.ts
@@ -6,19 +6,21 @@ const router =Router();
 //This the route for getting all the posts in the front page
 router.get("/getall/:skip",async (req:Request,res:Response) =>{
     try {
-        const post = await Post.find({}).skip(+req.params.skip).limit(10).lean().exec();
+        const post = await Post.find({}).skip(+req.params.skip*5).limit(5).lean().exec();
         return res.status(200).json(post);
     } catch (error) {
         return  res.status(500).send({message:error.message,status:"Failed"})
     }
 })
 
-// This is the route for getting all the posts of a perticular user
+// This is the route for getting  the posts of a perticular user with pagination
 
 router.get("/getbyuser/:userID/:skip", async (req:Request,res:Response) =>{
  try {
-     const post = await Post.find({})
- } catch (error) {
+     const post = await Post.find({author:req.params.userID}).skip(+req.params.skip*5)
+     .limit(5).lean().exec();
+     return res.status(200).json(post);
+    } catch (error) {
     return  res.status(500).send({message:error.message,status:"Failed"})
  }
 })
